1799976e99a082da54609ce5adfe092b62231333
0098-sayan <maitysayan704@gmail.com>
2025-03-11 23:01:35 +0530

    RestApi

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+PORT = 8000;
+
+app.get("/users", (req,res)=>{
+    const HTML=`<ul>${users.map((user)=>`<li>${user.first_name} ${user.last_name} ${user.email}</li>`).join('')}</ul>`;
+    res.send(HTML);
+})
+
+
+//Rest api
+app.get("/api/users",(req,res) =>{
+    return res.json(users);
+})
+
+app.get("/api/users/:id", (req,res)=>{
+    const id = Number(req.params.id);
+    const user= users.find((user)=>user.id === id);
+    return res.json(user);
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
