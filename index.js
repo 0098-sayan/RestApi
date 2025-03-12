6c882fb0786e8609ac208d149ca82fb1dd72d2a9
0098-sayan <maitysayan704@gmail.com>
2025-03-12 23:50:15 +0530

    Updated post method

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-const express = require('express');
+const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 PORT = 8000;
 
-app.get("/users", (req,res)=>{
-    const HTML=`<ul>${users.map((user)=>`<li>${user.first_name} ${user.last_name} </li>`).join('')}</ul>`;
-    res.send(HTML);
-})
+app.use(express.urlencoded({ extended: false }));
 
-app.post("/user", (req,res)=>{
-    res.send("Post request");
-})
-
-//Rest api
-app.get("/api/users",(req,res) =>{
-    return res.json(users);
-})
-
-app.get("/api/users/:id", (req,res)=>{
+//crating a route
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
-    const user= users.find((user)=>user.id === id);
+    const user = users.find((user) => user.id === id);
     return res.json(user);
-})
+  })
+  .patch((req, res) => {
+    return res.send("Patch request");
+  })
+  .delete((req, res) => {
+    return res.send("Delete request");
+  });
+
+app.get("/users", (req, res) => {
+  const HTML = `<ul>${users
+    .map((user) => `<li>${user.first_name} ${user.last_name} </li>`)
+    .join("")}</ul>`;
+  res.send(HTML);
+});
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ id: users.length + 1, ...body });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Success", id: users.length });
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
